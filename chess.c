da352d9e5f4bf89c4bbd8e644eff53572f75b7de
Lu <lucandic@wilkinson.cs.grinnell.edu>
2024-12-07 14:48:10 -0600

    changed chess.c to account for collisions, added comprehensive move validation function

--- chess.c
+++ chess.c
@@ -4,14 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <uchar.h>
+#include <unistd.h>
 
 #define BOARD_DIM 8
 #define BLOCK SIZE 32
 
-/** 
- * Piece Definitions 
+/**
+ * Piece Definitions
  */
 #define B_ROOK 0x0000265C
 #define B_KNIGHT 0x0000265E
@@ -37,7 +37,6 @@ typedef struct node {
   int row;
   int col;
   char32_t piece;
-  char piece_letter;
   int alignment;
 } node_t;
 
@@ -52,28 +51,28 @@ typedef struct board {
 char32_t b_pieces[] = {B_ROOK, B_KNIGHT, B_BISHOP, B_QUEEN, B_KING, B_BISHOP, B_KNIGHT, B_ROOK};
 char pieces_letter[] = {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'};
 char32_t w_pieces[] = {W_ROOK, W_KNIGHT, W_BISHOP, W_QUEEN, W_KING, W_BISHOP, W_KNIGHT, W_ROOK};
-char32_t even_row[] = {LIGHT_SPACE, DARK_SPACE, LIGHT_SPACE, DARK_SPACE, LIGHT_SPACE, DARK_SPACE, LIGHT_SPACE, DARK_SPACE};
-char32_t odd_row[] = {DARK_SPACE, LIGHT_SPACE, DARK_SPACE, LIGHT_SPACE, DARK_SPACE, LIGHT_SPACE, DARK_SPACE, LIGHT_SPACE};
+char32_t even_row[] = {LIGHT_SPACE, DARK_SPACE, LIGHT_SPACE, DARK_SPACE,
+                       LIGHT_SPACE, DARK_SPACE, LIGHT_SPACE, DARK_SPACE};
+char32_t odd_row[] = {DARK_SPACE, LIGHT_SPACE, DARK_SPACE, LIGHT_SPACE,
+                      DARK_SPACE, LIGHT_SPACE, DARK_SPACE, LIGHT_SPACE};
 
 /**
  * Initializes board to starting state
- * 
+ *
  * ALIGNMENT KEY:
  * 0 = EMPTY
  * 1 = BLACK PIECE
  * 2 = WHITE PIECE
  */
-void initialize (board_t* board) {
+void initialize(board_t* board) {
   // First row: Black Non-Pawn Pieces
   for (int i = 0; i < 8; i++) {
-    board->cells[i]->piece =  b_pieces[i];
-    board->cells[i]->piece_letter = pieces_letter[i];
+    board->cells[i]->piece = b_pieces[i];
     board->cells[i]->alignment = 1;
   }
   // Second row: Black Pawn Pieces
   for (int i = 8; i < 16; i++) {
     board->cells[i]->piece = B_PAWN;
-    board->cells[i]->piece_letter = 'P';
     board->cells[i]->alignment = 1;
   }
 
@@ -91,22 +90,86 @@ void initialize (board_t* board) {
   // Seventh row: White Pawn Pieces
   for (int i = 48; i < 56; i++) {
     board->cells[i]->piece = W_PAWN;
-    board->cells[i]->piece_letter = 'P';
     board->cells[i]->alignment = 2;
   }
 
   // Eighth row: White Non-Pawn Pieces
   for (int i = 56; i < 64; i++) {
     board->cells[i]->piece = w_pieces[i];
-    board->cells[i]->piece_letter = pieces_letter[63 - i];
     board->cells[i]->alignment = 2;
   }
 }
 
-/** 
+int validate_move(board_t* board, int startpos, int endpos, int myalign) {
+  char32_t mypiece = board->cells[startpos]->piece;
+  int* moves = NULL;
+
+  if (board->cells[startpos]->alignment != myalign) { // If alignment does not match up
+    return -1;
+  }
+
+  if (myalign == 1) {  // Your piece is black
+    switch (mypiece) {
+      case B_BISHOP:
+        moves = bishopmoves(startpos / 8, startpos % 8, board, myalign);
+        break;
+      case B_KING:
+        moves = kingmoves(startpos / 8, startpos % 8, board, myalign);
+        break;
+      case B_KNIGHT:
+        moves = knightmoves(startpos / 8, startpos % 8, board, myalign);
+        break;
+      case B_PAWN:
+        moves = black_pawnmoves(startpos / 8, startpos % 8, board);
+        break;
+      case B_QUEEN:
+        moves = queenmoves(startpos / 8, startpos % 8, board, myalign);
+        break;
+      case B_ROOK:
+        moves = rookmoves(startpos / 8, startpos % 8, board, myalign);
+        break;
+    }
+  } else if (myalign == 2) {  // Your piece is white
+    switch (mypiece) {
+      case W_BISHOP:
+        moves = bishopmoves(startpos / 8, startpos % 8, board, myalign);
+        break;
+      case W_KING:
+        moves = kingmoves(startpos / 8, startpos % 8, board, myalign);
+        break;
+      case W_KNIGHT:
+        moves = knightmoves(startpos / 8, startpos % 8, board, myalign);
+        break;
+      case W_PAWN:
+        moves = white_pawnmoves(startpos / 8, startpos % 8, board);
+        break;
+      case W_QUEEN:
+        moves = queenmoves(startpos / 8, startpos % 8, board, myalign);
+        break;
+      case W_ROOK:
+        moves = rookmoves(startpos / 8, startpos % 8, board, myalign);
+        break;
+    }
+  }
+
+  for (int i = 0; i < 27; i++) { // Check if prompted endpos is within validmoves
+    if (moves[i] == NULL) { // Reached end of list, break out of loop
+      break;
+    }
+    if (endpos == moves[i]) { // Move found
+      move_piece(board, startpos, endpos); // Make the move
+      return 1; // Return 1 for success
+    }
+  }
+
+  // If nothing returned, failed
+  return -1; 
+}
+
+/**
  * Helper Function: Making a move (ASSUMES IT IS VALID)
  */
-void move_piece (board_t* board, int startpos, int endpos) {
+void move_piece(board_t* board, int startpos, int endpos) {
   // Moves the Piece
   board->cells[endpos]->piece = board->cells[startpos]->piece;
   board->cells[endpos]->alignment = board->cells[startpos]->alignment;
@@ -114,9 +177,9 @@ void move_piece (board_t* board, int startpos, int endpos) {
   // Resets the original space to an empty node
   board->cells[startpos]->alignment = 0;
   if ((startpos / 8) % 2 == 0) {
-    board->cells[startpos]->piece = even_row[i % 8];
+    board->cells[startpos]->piece = even_row[startpos % 8];
   } else {
-    board->cells[startpos]->piece = odd_row[i % 8];
+    board->cells[startpos]->piece = odd_row[startpos % 8];
   }
 
   // Edge Case: If it is a pawn moving to the other edge
@@ -132,25 +195,67 @@ void move_piece (board_t* board, int startpos, int endpos) {
  * Params: Starting row, Starting Column
  * Return: Array of integers indicating possible positions to move to
  */
-int* rookmoves (int row, int col) {
-  // Define Starting Position
-  int start = (row * 8) + col;
-
-  // A rook will always have 14 available spaces to move to (NO TOWERING bc i genuinely cannot be bothered lmao)
+int* rookmoves(int row, int col, board_t* board, int myalign) {
+  // A rook will always have 14 available spaces to move to (NO TOWERING bc i genuinely cannot be
+  // bothered lmao)
   int* moves = malloc(sizeof(int) * 14);
   size_t counter = 0;
 
-  // Horizontal Movements in its own row
-  for (int i = row * 8; i < (row * 8) + 8; i++) {
-    if (i != start) {
+  // Rightward Movements in its own row
+  for (int i = col + 1; i < 8; i++) {
+    int pos = (row * 8) + i;
+    if (board->cells[pos]->alignment == myalign) {
+      break;
+    } else if (board->cells[pos]->alignment != 0) {
+      moves[counter] = pos;
+      counter++;
+      break;
+    } else {
       moves[counter] = i;
       counter++;
     }
   }
 
-  // Vertical Movements across rows
-  for (int i = col; i < 64; i += 8) {
-    if (i != start) {
+  // Leftward Movements in its own row
+  for (int i = col - 1; i >= 0; i--) {
+    int pos = (row * 8) + i;
+    if (board->cells[pos]->alignment == myalign) {
+      break;
+    } else if (board->cells[pos]->alignment != 0) {
+      moves[counter] = pos;
+      counter++;
+      break;
+    } else {
+      moves[counter] = i;
+      counter++;
+    }
+  }
+
+  // Upward Movements across rows
+  for (int i = row - 1; i >= 0; i--) {
+    int pos = (i * 8) + col;
+    if (board->cells[pos]->alignment == myalign) {
+      break;
+    } else if (board->cells[pos]->alignment != 0) {
+      moves[counter] = pos;
+      counter++;
+      break;
+    } else {
+      moves[counter] = i;
+      counter++;
+    }
+  }
+
+  // Downward Movements across rows
+  for (int i = row + 1; i < 8; i++) {
+    int pos = (i * 8) + col;
+    if (board->cells[pos]->alignment == myalign) {
+      break;
+    } else if (board->cells[pos]->alignment != 0) {
+      moves[counter] = pos;
+      counter++;
+      break;
+    } else {
       moves[counter] = i;
       counter++;
     }
@@ -159,10 +264,7 @@ int* rookmoves (int row, int col) {
   return moves;
 }
 
-int* knightmoves (int row, int col) {
-  // Define Starting Position
-  int start = (row * 8) + col;
-
+int* knightmoves(int row, int col, board_t* board, int myalign) {
   // A knight will have a maximum of 8 available spaces to move to
   int* moves = malloc(sizeof(int) * 8);
   size_t counter = 0;
@@ -174,58 +276,114 @@ int* knightmoves (int row, int col) {
   for (int i = 0; i < 8; i++) {
     if (rows[i] >= 0 && rows[i] <= 7 && cols[i] >= 0 && cols[i] <= 7) {
       int endpos = (rows[i] * 8) + cols[i];
-      moves[counter] = endpos;
-      counter++;
+      if (board->cells[endpos]->alignment != myalign) {
+        moves[counter] = endpos;
+        counter++;
+      }
     }
   }
 
   return moves;
 }
 
-int* bishopmoves (int row, int col) {
-  // Define Starting Position
-  int start = (row * 8) + col;
-
+int* bishopmoves(int row, int col, board_t* board, int myalign) {
   // A bishop will have a maximum of 13 available spaces to move to
   int* moves = malloc(sizeof(int) * 13);
   size_t counter = 0;
 
-  // Direction 1 Movements
-  for (int i = -7; i < 7; i++) {
-    int newrow = row + i;
-    int newcol = col - i;
+  // UP + RIGHT Movements
+  for (int i = 1; i < 7; i++) {
+    int newrow = row - i;
+    int newcol = col + i;
     if (newrow >= 0 && newrow <= 7 && newcol >= 0 && newcol <= 7) {
       int endpos = (newrow * 8) + newcol;
-      if (endpos != start) {
+      if (board->cells[endpos]->alignment == myalign) {
+        break;
+      } else if (board->cells[endpos]->alignment != 0) {
+        moves[counter] = endpos;
+        counter++;
+        break;
+      } else {
         moves[counter] = endpos;
         counter++;
       }
+    } else {
+      break;
     }
-    
   }
 
-  // Direction 2 Movements
-  for (int i = -7; i < 7; i++) {
+  // UP + LEFT Movements
+  for (int i = 1; i < 7; i++) {
+    int newrow = row - i;
+    int newcol = col - i;
+    if (newrow >= 0 && newrow <= 7 && newcol >= 0 && newcol <= 7) {
+      int endpos = (newrow * 8) + newcol;
+      if (board->cells[endpos]->alignment == myalign) {
+        break;
+      } else if (board->cells[endpos]->alignment != 0) {
+        moves[counter] = endpos;
+        counter++;
+        break;
+      } else {
+        moves[counter] = endpos;
+        counter++;
+      }
+    } else {
+      break;
+    }
+  }
+
+  // DOWN + RIGHT Movements
+  for (int i = 1; i < 7; i++) {
     int newrow = row + i;
     int newcol = col + i;
     if (newrow >= 0 && newrow <= 7 && newcol >= 0 && newcol <= 7) {
       int endpos = (newrow * 8) + newcol;
-      if (endpos != start) {
+      if (board->cells[endpos]->alignment == myalign) {
+        break;
+      } else if (board->cells[endpos]->alignment != 0) {
+        moves[counter] = endpos;
+        counter++;
+        break;
+      } else {
         moves[counter] = endpos;
         counter++;
       }
+    } else {
+      break;
+    }
+  }
+
+  // DOWN + LEFT Movements
+  for (int i = 1; i < 7; i++) {
+    int newrow = row + i;
+    int newcol = col - i;
+    if (newrow >= 0 && newrow <= 7 && newcol >= 0 && newcol <= 7) {
+      int endpos = (newrow * 8) + newcol;
+      if (board->cells[endpos]->alignment == myalign) {
+        break;
+      } else if (board->cells[endpos]->alignment != 0) {
+        moves[counter] = endpos;
+        counter++;
+        break;
+      } else {
+        moves[counter] = endpos;
+        counter++;
+      }
+    } else {
+      break;
     }
   }
 
   return moves;
 }
 
-int* queenmoves (int row, int col) {
+int* queenmoves(int row, int col, board_t* board, int myalign) {
   // A queen will have a maximum of 27 available spaces to move to
   int* moves = malloc(sizeof(int) * 27);
 
-  int* straightmoves = rookmoves(row, col);
-  int* diagonalmoves = bishopmoves(row, col);
+  int* straightmoves = rookmoves(row, col, board, myalign);
+  int* diagonalmoves = bishopmoves(row, col, board, myalign);
 
   memcpy(moves, straightmoves, 14 * sizeof(int));
   memcpy(moves, diagonalmoves, 13 * sizeof(int));
@@ -233,9 +391,10 @@ int* queenmoves (int row, int col) {
   return moves;
 }
 
-int* kingmoves (int row, int col) {
+int* kingmoves(int row, int col, board_t* board, int myalign) {
   // A King will have a maximum of 8 available spaces to move to
   int* moves = malloc(sizeof(int) * 8);
+  int counter = 0;
 
   int rows[] = {row - 1, row - 1, row, row + 1, row + 1, row + 1, row, row - 1};
   int cols[] = {col, col + 1, col + 1, col + 1, col, col - 1, col - 1, col - 1};
@@ -243,32 +402,37 @@ int* kingmoves (int row, int col) {
   for (int i = 0; i < 8; i++) {
     if (rows[i] >= 0 && rows[i] <= 7 && cols[i] >= 0 && cols[i] <= 7) {
       int endpos = (rows[i] * 8) + cols[i];
-      moves[counter] = endpos;
-      counter++;
+      if (board->cells[endpos]->alignment != myalign) {
+        moves[counter] = endpos;
+        counter++;
+      }
     }
   }
 
   return moves;
 }
 
-int* white_pawnmoves (int row, int col, board_t* board) {
+int* white_pawnmoves(int row, int col, board_t* board) {
   // A pawn will have a maximum of 4 available spaces to move to
   int* moves = malloc(sizeof(int) * 4);
   int counter = 0;
 
   // Check if it is in first row
   if (row == 6) {
-    moves[counter] = ((row - 2) * 8) + col;
-    counter++;
+    int doublemove = ((row - 2) * 8) + col;
+    if (board->cells[doublemove]->alignment == 0) {
+      moves[counter] = doublemove;
+      counter++;
+    }
   }
 
   // Check if there are pieces diagonally in front of it
-  
+
   // Up-Left
   if (row - 1 >= 0 && row - 1 <= 7 && col - 1 >= 0 && col - 1 <= 7) {
     int upleft = ((row - 1) * 8) + (col - 1);
     if (board->cells[upleft]->alignment == 1) {
-      moves[counter] == upleft;
+      moves[counter] = upleft;
       counter++;
     }
   }
@@ -277,38 +441,44 @@ int* white_pawnmoves (int row, int col, board_t* board) {
   if (row - 1 >= 0 && row - 1 <= 7 && col + 1 >= 0 && col + 1 <= 7) {
     int upleft = ((row - 1) * 8) + (col + 1);
     if (board->cells[upleft]->alignment == 1) {
-      moves[counter] == upleft;
+      moves[counter] = upleft;
       counter++;
     }
   }
 
   // Normal case
   if (row - 1 >= 0) {
-    moves[counter] = ((row - 1) * 8) + col;
-    counter++;
+    int normalmove = ((row - 1) * 8) + col;
+    if (board->cells[normalmove]->alignment == 0) {
+      moves[counter] = ((row - 1) * 8) + col;
+      counter++;
+    }
   }
-  
+
   return moves;
 }
 
-int* black_pawnmoves (int row, int col, board_t* board) {
+int* black_pawnmoves(int row, int col, board_t* board) {
   // A pawn will have a maximum of 4 available spaces to move to
   int* moves = malloc(sizeof(int) * 4);
   int counter = 0;
 
   // Check if it is in first row
   if (row == 1) {
-    moves[counter] = ((row + 2) * 8) + col;
-    counter++;
+    int doublemove = ((row + 2) * 8) + col;
+    if (board->cells[doublemove] == 0) {
+      moves[counter] = doublemove;
+      counter++;
+    }
   }
 
   // Check if there are pieces diagonally in front of it
-  
+
   // Up-Left
   if (row + 1 >= 0 && row + 1 <= 7 && col - 1 >= 0 && col - 1 <= 7) {
     int upleft = ((row + 1) * 8) + (col - 1);
-    if (board->cells[upleft]->alignment == 1) {
-      moves[counter] == upleft;
+    if (board->cells[upleft]->alignment == 2) {
+      moves[counter] = upleft;
       counter++;
     }
   }
@@ -316,17 +486,20 @@ int* black_pawnmoves (int row, int col, board_t* board) {
   // Up-Right
   if (row + 1 >= 0 && row + 1 <= 7 && col + 1 >= 0 && col + 1 <= 7) {
     int upleft = ((row + 1) * 8) + (col + 1);
-    if (board->cells[upleft]->alignment == 1) {
-      moves[counter] == upleft;
+    if (board->cells[upleft]->alignment == 2) {
+      moves[counter] = upleft;
       counter++;
     }
   }
 
   // Normal case
   if (row + 1 <= 7) {
-    moves[counter] = ((row + 1) * 8) + col;
-    counter++;
+    int normalmove = ((row + 1) * 8) + col;
+    if (board->cells[normalmove]->alignment == 0) {
+      moves[counter] = normalmove;
+      counter++;
+    }
   }
-  
+
   return moves;
 }
